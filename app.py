c2fc1318754e43a5a0abea36b38905437cbb92d6
Sabyasachi Bhoi <sabyabhoi@gmail.com>
2023-04-22 09:51:18 +0530

    Add simple menu for IO

--- app.py
+++ app.py
@@ -6,13 +6,13 @@ def get_db(filename):
 
     con.sql('''CREATE TABLE IF NOT EXISTS CASH_INFLOW(
     IID INTEGER PRIMARY KEY,
-    CATEGORY VARCHAR(20) NOT NULL
+    CATEGORY VARCHAR(50) NOT NULL
     )''')
     con.sql('CREATE SEQUENCE IF NOT EXISTS seq_inflowid START 1')
 
     con.sql('''CREATE TABLE IF NOT EXISTS CASH_OUTFLOW(
     OID INTEGER PRIMARY KEY,
-    CATEGORY VARCHAR(20) NOT NULL
+    CATEGORY VARCHAR(50) NOT NULL
     )''')
     con.sql('CREATE SEQUENCE IF NOT EXISTS seq_outflowid START 1')
 
@@ -23,8 +23,9 @@ def create_cash_flow(con, name, inflow=True):
     table_name = 'CASH_INFLOW' if inflow else 'CASH_OUTFLOW'
     seq_name = 'seq_inflowid' if inflow else 'seq_outflowid'
 
-    con.execute("SELECT * FROM " + table_name +
-                " WHERE UPPER(CATEGORY) LIKE ?", [name])
+    con.execute(
+        "SELECT * FROM " + table_name + " WHERE UPPER(CATEGORY) LIKE ?",
+        [name])
 
     if len(con.fetchall()) == 0:
         con.execute("INSERT INTO " + table_name + " VALUES(nextVal(?), ?)",
@@ -34,9 +35,24 @@ def create_cash_flow(con, name, inflow=True):
     con.table(table_name).show()
 
 
+def menu(con):
+    print('Enter option:')
+    print('1. Create cash inflow')
+    print('2. Create cash outflow')
+    chosen = int(input().strip())
+    if chosen == 1:
+        print('Enter inflow name: ', end='')
+        name = input().strip()
+        create_cash_flow(con, name, inflow=True)
+    elif chosen == 2:
+        print('Enter outflow name: ', end='')
+        name = input().strip()
+        create_cash_flow(con, name, inflow=False)
+
+
 if __name__ == '__main__':
     con = get_db('file.db')
-    create_cash_flow(con, 'AR')
+    menu(con)
 
     con.commit()
     con.close()
